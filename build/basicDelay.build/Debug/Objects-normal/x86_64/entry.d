83871d3ca1d89aa65df220fb64ddb06e729ae3fa
Alfie Bradic <alfiebradic@googlemail.com>
2023-03-15 14:34:44 +0000

    Added cmakelists.txt

--- build/basicDelay.build/Debug/Objects-normal/x86_64/entry.d
+++ build/basicDelay.build/Debug/Objects-normal/x86_64/entry.d
@@ -1,21 +1,23 @@
 dependencies: /Users/alfiebradic/Documents/basicDelay/source/entry.cpp \
   /Users/alfiebradic/Documents/basicDelay/source/processor.h \
-  /Users/alfiebradic/vst3sdk/public.sdk/source/vst/vstaudioeffect.h \
-  /Users/alfiebradic/vst3sdk/public.sdk/source/vst/vstbus.h \
-  /Users/alfiebradic/vst3sdk/base/source/fobject.h \
+  /Users/alfiebradic/Documents/basicDelay/source/cids.h \
   /Users/alfiebradic/vst3sdk/pluginterfaces/base/funknown.h \
   /Users/alfiebradic/vst3sdk/pluginterfaces/base/fplatform.h \
   /Users/alfiebradic/vst3sdk/pluginterfaces/base/ftypes.h \
   /Users/alfiebradic/vst3sdk/pluginterfaces/base/smartpointer.h \
+  /Users/alfiebradic/vst3sdk/pluginterfaces/vst/vsttypes.h \
+  /Users/alfiebradic/vst3sdk/pluginterfaces/base/fstrdefs.h \
+  /Users/alfiebradic/Documents/basicDelay/source/ringbuffer.h \
+  /Users/alfiebradic/vst3sdk/public.sdk/source/vst/vstaudioeffect.h \
+  /Users/alfiebradic/vst3sdk/public.sdk/source/vst/vstbus.h \
+  /Users/alfiebradic/vst3sdk/base/source/fobject.h \
   /Users/alfiebradic/vst3sdk/pluginterfaces/base/iupdatehandler.h \
   /Users/alfiebradic/vst3sdk/base/source/fdebug.h \
   /Users/alfiebradic/vst3sdk/base/source/fstring.h \
-  /Users/alfiebradic/vst3sdk/pluginterfaces/base/fstrdefs.h \
   /Users/alfiebradic/vst3sdk/pluginterfaces/base/istringresult.h \
   /Users/alfiebradic/vst3sdk/pluginterfaces/base/ipersistent.h \
   /Users/alfiebradic/vst3sdk/pluginterfaces/vst/ivstcomponent.h \
   /Users/alfiebradic/vst3sdk/pluginterfaces/base/ipluginbase.h \
-  /Users/alfiebradic/vst3sdk/pluginterfaces/vst/vsttypes.h \
   /Users/alfiebradic/vst3sdk/pluginterfaces/base/falignpush.h \
   /Users/alfiebradic/vst3sdk/pluginterfaces/base/falignpop.h \
   /Users/alfiebradic/vst3sdk/pluginterfaces/vst/ivstaudioprocessor.h \
@@ -34,7 +36,6 @@ dependencies: /Users/alfiebradic/Documents/basicDelay/source/entry.cpp \
   /Users/alfiebradic/vst3sdk/public.sdk/source/common/pluginview.h \
   /Users/alfiebradic/vst3sdk/pluginterfaces/gui/iplugview.h \
   /Users/alfiebradic/vst3sdk/pluginterfaces/base/typesizecheck.h \
-  /Users/alfiebradic/Documents/basicDelay/source/cids.h \
   /Users/alfiebradic/Documents/basicDelay/source/version.h \
   /Users/alfiebradic/Documents/basicDelay/build/projectversion.h \
   /Users/alfiebradic/vst3sdk/public.sdk/source/main/pluginfactory.h
